61938f0a9e628530186b76616c68c4d8f2ea1f90
alaketych <alaketych@ukr.net>
2020-04-27 22:07:34 +0300

    server configuration

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const path = require('path')
+const multer = require('multer')
 const express = require('express')
+const bodyParser = require('body-parser')
 const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
 app.use(express.static(__dirname + '/public'));
+
+const storage = multer.diskStorage({
+  destination: '.public/uploads/',
+  filename: (request,  file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.extname))
+  }
+})
+
+const upload = multer({
+  storage: storage
+}).single('uploadImage');
 
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'layout',
@@ -17,7 +30,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'handlebars')
 
 
-const startPage = require('./routes/startPage')
+
+const startPage = require('./routes/routes')
 app.use('/', startPage)
 
 app.set("port", process.env.PORT || 8000);
