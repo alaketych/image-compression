b3fd02c3a5cf5fab7b0934efa5ae14178ce501d4
alaketych <alaketych@ukr.net>
2020-05-06 19:52:59 +0300

    js files

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,3 +1,12 @@
 function closeMessage() {
     document.getElementById('foo').style.display='none'
 }
+
+const typeCompression = document.getElementsByName("typeCompression");
+for (let radio of typeCompression) {
+    radio.onchange = radio_change;
+}
+
+function radio_change() {
+    alert(this.value);
+}
