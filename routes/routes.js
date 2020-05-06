18487878581f2fb9da1e454417ae39eb8659aab7
alaketych <alaketych@ukr.net>
2020-05-06 19:51:56 +0300

    http routing

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,24 @@ const express   = require('express')
 const multer    = require('multer')
 const router    = express.Router()
 
+const imagemin          = require('imagemin')
+const imageminMozjpeg   = require('imagemin-mozjpeg')   //lossy jpeg
+const imageminJpegtran  = require('imagemin-jpegtran')  //lossless jpeg
+const imageminPngquant  = require('imagemin-pngquant')  //lossy png
+const imageminOptipng   = require('imagemin-optipng')   //lossless png
+const imageminGiflossy  = require('imagemin-giflossy')  //lossy gif
+const imageminGifsicle  = require('imagemin-gifsicle')  //lossless gif
+const imageminSvgo      = require('imagemin-svgo')      //lossy svg
+const imageminWebp      = require('imagemin-webp')      //lossy webp
+
+const Vibrant   = require('node-vibrant')
+const RLE       = require('../alghoritms/rle')
+
+function imageBase64(file) {
+    var bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
+}
+
 router.use(express.static(__dirname + '/public'))
 
 const storage = multer.diskStorage({
@@ -26,7 +44,7 @@ const upload = multer({
            ext !== '.raw'   && ext !== '.arw'   && ext !== '.cr2'  && ext !== '.hrw'  && ext !== '.k25'  &&                       //raw
            ext !== '.ind'   && ext !== '.indd'  && ext !== '.indt' &&                                                             //indd
            ext !== '.bmp'   && ext !== '.dip'   &&                                                                                //bmp
-           ext !== '.heif'  && ext !== '.heic') {                                                                               //heif
+           ext !== '.heif'  && ext !== '.heic') {                                                                                 //heif
             return callback(new Error)
         }
         callback(null, true)
@@ -35,6 +53,12 @@ const upload = multer({
 
 router.get('/', (request, response) => {
     response.render('upload')
+})
+
+router.get('/result/:filename', (request, response) => {
+    response.render('result', {
+        compressedImage: compressedImage
+    })
 })
 
 router.post('/upload', (request, response) => {
@@ -61,20 +85,47 @@ router.post('/upload', (request, response) => {
 })
 
 router.get('/:filename', (request, response) => {
-    let fileName = request.params.filename;
+    let fileName = request.params.filename
 
-    let pathToCheck = path.join(__dirname, '../public/uploads/' + fileName);
-    //console.log('pathToCheck==', pathToCheck);
-
+    let pathToCheck = path.join(__dirname, '../public/uploads/' + fileName)
     if (fs.existsSync(pathToCheck)) {
         let imageDestination = `${request.protocol}://${request.headers.host}/uploads/${fileName}`;
         response.render('compress', {
             userImage: imageDestination
         })
+
+        var bitmap = imageBase64(pathToCheck);
+        const encode = RLE.Encode(bitmap)
+
+        Vibrant.from(pathToCheck).getPalette()
+                .then(palette => {
+                    console.log(palette)
+                })
     }
     else {
         response.send('Oops.')
     }
+})
+
+router.post('/compress', async (request, response) => {
+    const compressedImage = await imagemin(['uploads/' + request.params.filename], {
+        destination: 'compressed',
+        plugins: [
+            imageminMozjpeg(),
+            imageminJpegtran(),
+
+            imageminPngquant(),
+            imageminOptipng(),
+
+            imageminGiflossy(),
+            imageminGifsicle(),
+
+            imageminSvgo(),
+            imageminWebp()
+        ]
+    })
+
+    response.redirect(`/result/${request.file.filename}`)
 })
 
 module.exports = router
